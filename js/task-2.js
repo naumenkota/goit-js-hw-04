57308ba3ab98a9a1fe9c015971295427b3db0876
Tetiana Naumenko <naumenko.ta@gmail.com>
2024-12-21 18:54:49 +0200

    add JS

---
+++ js/task-2.js
@@ -0,0 +1,42 @@
+function calcAverageCalories(days) {
+    const caloriesArrey = [];
+    for (const values of days) {
+        caloriesArrey.push(values.calories)
+    }
+    let sum = 0;
+    for (const dayValues of caloriesArrey) {
+        sum += dayValues;
+    }
+    return caloriesArrey.length ===0 ? 0 : sum / caloriesArrey.length;
+}
+    
+
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
